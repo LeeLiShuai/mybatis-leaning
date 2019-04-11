5f312ffea769763093e524b21e5942bb9ba3a3c8
969746481@qq.com <969746481@qq.com>
2019-04-11 19:56:30 +0800

    '类型转化'

--- src/main/java/org/apache/ibatis/type/IntegerTypeHandler.java
+++ src/main/java/org/apache/ibatis/type/IntegerTypeHandler.java
@@ -1,17 +1,15 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
+ * Copyright 2009-2018 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.apache.ibatis.type;
 
@@ -21,10 +19,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
+ * int类型转化类
+ *
  * @author Clinton Begin
  */
 public class IntegerTypeHandler extends BaseTypeHandler<Integer> {
 
+  /**
+   * 调用驱动的setInt方法将int转化为jdbcType的int
+   */
   @Override
   public void setNonNullParameter(PreparedStatement ps, int i, Integer parameter, JdbcType jdbcType)
       throws SQLException {
--- src/main/java/org/apache/ibatis/type/TypeReference.java
+++ src/main/java/org/apache/ibatis/type/TypeReference.java
@@ -1,17 +1,15 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
+ * Copyright 2009-2016 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.apache.ibatis.type;
 
@@ -22,18 +20,18 @@ import java.lang.reflect.Type;
  * References a generic type.
  *
  * @param <T> the referenced type
- * @since 3.1.0
  * @author Simone Tripodi
+ * @since 3.1.0
  */
 public abstract class TypeReference<T> {
 
   private final Type rawType;
 
-  protected TypeReference() {
+  TypeReference() {
     rawType = getSuperclassTypeParameter(getClass());
   }
 
-  Type getSuperclassTypeParameter(Class<?> clazz) {
+  private Type getSuperclassTypeParameter(Class<?> clazz) {
     Type genericSuperclass = clazz.getGenericSuperclass();
     if (genericSuperclass instanceof Class) {
       // try to climb up the hierarchy until meet something useful
@@ -41,8 +39,9 @@ public abstract class TypeReference<T> {
         return getSuperclassTypeParameter(clazz.getSuperclass());
       }
 
-      throw new TypeException("'" + getClass() + "' extends TypeReference but misses the type parameter. "
-        + "Remove the extension or add a type parameter to it.");
+      throw new TypeException(
+          "'" + getClass() + "' extends TypeReference but misses the type parameter. "
+              + "Remove the extension or add a type parameter to it.");
     }
 
     Type rawType = ((ParameterizedType) genericSuperclass).getActualTypeArguments()[0];
